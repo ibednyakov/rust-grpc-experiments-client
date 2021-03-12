f708b11f7b5935c19efcb7cd18453d034ba13060
ibednyakov <ivan.bednyakov@gmail.com>
2021-03-12 08:47:59 +0100

    GRPC with streaming - not tested yet

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use futures::stream::iter;
 use tonic::Request;
 
+use std::{thread, time, io};
+
 use common::ss_user_update;
 #[allow(unused_imports)]
 use common::ss_response;
@@ -71,14 +73,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         opt_is_continuation: Option::default(),
     });
 
+    println!("For exit press 1<Enter>");
     println!("Trying to open a session...");
     let response = client.open(request).await?.into_inner();
 
-    // while let Some(res) = response.message().await? {
-    //     println!("NOTE = {:?}", res);
-    // }
+    let mut input = String::new();
+    let ten_millis = time::Duration::from_millis(10);
+
+    while let _res = io::stdin().read_line(&mut input) {
+        // io::stdin().read_line(&mut input)?;
+        let mut _parse_res = match input.trim().parse::<u32>() {
+            Ok(num) => {
+                match num {
+                    1 => break,
+                    _ => println!("Enter 1 for exit. Read {}", input),
+                };
+            }
+            Err(_error) => {
+                println!("Enter 1 for exit. Read {}", input);
+                continue;
+            }
+        };
+        thread::sleep(ten_millis);
+    }
 
     println!("RESPONSE: session ID = {}", response.session_id);
+    let request_close = tonic::Request::new(SsRequest {
+        user_id: 123,
+        msg_timestamp: 9878374,
+        user_hash: String::from("blah-blah-blah"),
+        start_point: Option::from( SsUserUpdate {
+            latitude: 0.0,
+            longitude: 0.0,
+            location_timestamp: 23984794,
+            status: ss_user_update::Status::Passive as i32,
+        } ),
+        opt_is_continuation: Option::default(),
+    });
+    let _close_res = client.close(request_close).await?.into_inner();
 
     Ok(())
 }
